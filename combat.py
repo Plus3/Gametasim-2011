047fec7512a5d6d59a11d0da056cef744c609bc9
Andrei Z <b1naryth1ef@gmail.com>
2011-09-23 02:56:08 -0500

    Updated events to use functions instead of long if statements. Trimmed combat into one function with an if for the exit command.

--- combat.py
+++ combat.py
@@ -28,69 +28,7 @@ def checkHealth(obj):
 	else:
 		return False
 
-def Defense(player, attacker, data):
-	meDead = checkHealth(player)
-	youDead = checkHealth(attacker)
-
-	while meDead is not True and youDead is not True:
-		todo = console()
-		doHit = True
-		if todo[0].startswith("use"):
-			try:
-				_item = int(todo[1])
-				if player.inv[_item] != None and player.inv[_item].weapon == True:
-					print "Using "+player.inv[_item].name
-					item = _item
-				elif _item == "fists":
-					pass
-				else:
-					print "Unknown Item"
-			except:
-				print "Unknown Item"
-		elif todo[0].startswith('inv'):
-			hitr = False
-			data['printInv'](player)
-		elif todo[0].startswith("a"):
-			if item is None:
-				print "You must select an item to use! If you don't have a sword try 'use fists'"
-			else:
-				use = player.use(item)
-				if use[0] == 1 and doHit != False:
-					hit(player, attacker, use[1])
-				else:
-					print "The weapon is broken!"
-					hit(player, attacker, 0, False)
-					citem = None
-		elif todo[0].startswith('exit'):
-			print "You try to run away...",
-			time.sleep(.9)
-			x = random.randint(1,5)
-			if x == 5:
-				print "You got away from",attacker.name+"!"
-				data['delBot'](attacker.name)
-				break
-			else:
-				print "Nice try!"
-				hit(player, attacker, 0, False)
-		else:
-			print "Unknown command!"
-			x = random.randint(1,5)
-			if x == 5:
-				hit(player,attacker,0,False)
-		
-		meDead = checkHealth(player)
-		youDead = checkHealth(attacker)
-
-	if meDead is True:
-		print "%s %s you!" %(attacker.name, wordy())
-		sys.exit()
-	elif youDead is True:
-		print "You %s %s" % (wordy(), attacker.name)
-		data['delBot'](attacker.name)
-
-	raw_input("[Exit]")
-
-def Offense(player, attacker, data):
+def Combat(player, attacker, mode, data):
 	meDead = checkHealth(player)
 	youDead = checkHealth(attacker)
 	item = None
@@ -125,12 +63,23 @@ def Offense(player, attacker, data):
 					hit(player, attacker, 0, False)
 					citem = None
 		elif todo[0].startswith('exit'):
-			print attacker.name, "runs away!"
-			data['delBot'](attacker.name)
-			break
+			if mode == "offense":
+				print attacker.name, "runs away!"
+				data['delBot'](attacker.name)
+				break
+			elif mode == "defense":
+				print "You try to run away...",
+				time.sleep(.9)
+				x = random.randint(1,5)
+				if x == 5:
+					print "You got away from",attacker.name+"!"
+					data['delBot'](attacker.name)
+					break
+				else:
+					print "Nice try!"
+					hit(player, attacker, 0, False)
 		else:
 			print "Unknown command!"
-
 		
 		meDead = checkHealth(player)
 		youDead = checkHealth(attacker)
@@ -146,10 +95,10 @@ def Offense(player, attacker, data):
 
 def battle(player, attacker, Map, attacked=True, data={}):
 	if attacked is True:
-		print "\n"+attacker.name, "initated an battle!"
-		Defense(player,attacker,data)
+		print "\n"+attacker.name, "initated a battle!"
+		Combat(player,attacker,"defense",data)
 	else:
 		print "\nAttacking", attacker.name
-		Offense(player,attacker,data)
+		Offense(player,attacker,"offense",data)
 
 
--- events.py
+++ events.py
@@ -1,9 +1,9 @@
 import sys, os, reqs
 
 class Event():
-	def __init__(self, pos, type, data, once):
+	def __init__(self, pos, kind, data, once):
 		self.pos = pos
-		self.type = type
+		self.kind = kind
 		self.data = data
 		self.once = once
 		self.fired = False
@@ -29,41 +29,29 @@ class Event():
 
 	def DOOR(self):
 		p = self.data['player']
+		im = None
 		go = False
+		for i in p.inv:
+			if p.inv[i] == None:
+				pass
+			elif p.inv[i].id == self.data['req']:
+				im = i
+				go = True
+
+		if go is True:
+			raw_input(self.data['msg2'])
+			p.inv[im] = None
+			self.once = True
+			if self.data['changeChar'][0] is True:
+				self.data['setChar'](self.data['changeChar'][2], self.pos, self.data['changeChar'][1])
+		else:	
+			p.pos = p.lastPos
+			raw_input(self.data['msg'])
 
 	def go(self):
 		self.fired = True
-
-		if self.type == "msg":
-			print self.data['msg']
-			raw_input("")
-		elif self.type == "end":
-			print self.data['msg']
-			sys.exit()
-		elif self.type == "pickup":
-			self.data['player'].pickupItem(self.data['item'])
-		elif self.type == "changemap":
-			self.data['setter'](self.data['map'], pos=self.data['pos'])
-		elif self.type == "door":
-			p = self.data['player']
-			im = None
-			go = False
-			for i in p.inv:
-				if p.inv[i] == None:
-					pass
-				elif p.inv[i].id == self.data['req']:
-					im = i
-					go = True
-
-			if go is True:
-				raw_input(self.data['msg2'])
-				p.inv[im] = None
-				self.once = True
-				if self.data['changeChar'][0] is True:
-					self.data['setChar'](self.data['changeChar'][2], self.pos, self.data['changeChar'][1])
-			else:	
-				p.pos = p.lastPos
-				raw_input(self.data['msg'])
+		if self.kind in self.types:
+			self.types[self.kind]()
 			
 	def fire(self):
 		if self.once is True:
